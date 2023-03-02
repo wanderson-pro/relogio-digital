fde719e086882d82740d78ccad35b737f5a9d286
José Wanderson <123521324+wanderson-prog@users.noreply.github.com>
2023-03-02 19:51:29 -0300

    Abreviação de código

--- relogiodigital.js
+++ relogiodigital.js
@@ -1,20 +1,13 @@
 function meuRelogio() {
-    let dateTempo = new Date()
-    let hora = dateTempo.getHours()
-    let minuto = dateTempo.getMinutes()
-    let segundo = dateTempo.getSeconds()
+    let dateTempo = new Date();
+    let hora = dateTempo.getHours();
+    let minuto = dateTempo.getMinutes();
+    let segundo = dateTempo.getSeconds();
 
-    if (hora < 10) {
-        hora = '0' + hora
-    }
+    hora = hora < 10 ? `0${hora}` : hora;
+    minuto = minuto < 10 ? `0${minuto}` : minuto;
+    segundo = segundo < 10 ? `0${segundo}` : segundo;
 
-    if (minuto < 10) {
-        minuto = '0' + minuto
-    }
-
-    if (segundo < 10) {
-        segundo = '0' + segundo
-    }
     document.getElementById('horas').innerHTML = hora
     document.getElementById('minutos').innerHTML = minuto
     document.getElementById('segundos').innerHTML = segundo
